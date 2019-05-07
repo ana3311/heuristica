ef08912e0f83cac5fa53cfaa9d530c59fa6c1852
ana3311 <anayara.2.10@hotmail.com>
2019-05-07 23:21:51 +0100

    Add files via upload

---
+++ Heuristica/run.py
@@ -0,0 +1,15 @@
+# Search methods
+# https://github.com/ana3311/heuristica.git
+import search
+
+ab = search.GPSProblem('A', 'B'
+                       , search.romania)
+
+print(search.breadth_first_graph_search(ab).path())
+print(search.depth_first_graph_search(ab).path())
+print(search.ramification_first_graph_search(ab).path())
+print(search.ramificationHeuristic_first_graph_search(ab).path())
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
